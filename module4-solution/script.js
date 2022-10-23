18c7e866ae325f73bf519905f80971b0d02fd13a
Adrian <caesar.adrian88@gmail.com>
2022-10-23 16:50:50 +0700

    Module 4 Solution

---
+++ module4-solution/script.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  if (names[i].charAt(0).toLowerCase() === 'j') {
+    byeSpeaker.sayBye(names[i]);
+  } else {
+    helloSpeaker.sayHello(names[i]);
+  }
+}
